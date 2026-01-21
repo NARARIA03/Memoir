1f8765cd68ba670030aca5ba9a7f246e04a41a52
NARARIA03 <hyeonseong0305@gmail.com>
2026-01-22 01:16:08 +0900

    feat: 미사용 변수, 타입 import 누락 ESLint 룰 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,32 @@ import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
+const typescriptTypeImportRule = {
+  files: ['**/*.ts', '**/*.tsx', '**/*.mjs', '**/*.mts'],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+};
+
+const noUnusedVarsRule = {
+  files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  typescriptTypeImportRule,
+  noUnusedVarsRule,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
